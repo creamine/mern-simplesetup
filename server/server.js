5814746c740f478c6fa11c78bfd233ced3edc3b0
creamine <kenfaoui@outlook.com>
2023-04-21 17:23:14 -0400

    Setting up the server with Express & Node

---
+++ server/server.js
@@ -0,0 +1,31 @@
+// File to setup the server
+
+import path from "path";
+import express from "express"; // Import express module
+import template from "./../template"; // Path to our app entry
+import devBundle from "./devBundle"; //comment out before building for production
+
+const app = express(); // initialize an express app to build out the rest of the Node server app
+devBundle.compile(app); //comment out before building for production
+
+// The folloing will configure the Express app to return static files from the dist folder when the requested route starts with /dist.
+const CURRENT_WORKING_DIR = process.cwd();
+app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+
+// The following route-handling code makes the Express app receive GET requests at /:
+app.get("/", (req, res) => {
+  res.status(200).send(template());
+});
+
+// Configuring the Express app to start a server that listens on the specified port for incoming requests:
+let port = process.env.PORT || 3000;
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info("Server started on port %s.", port);
+});
+
+// With this code, when the server is running, it will be able to accept requests
+// at the root route and render the React view with the "Hello World" text in the browser.
+// The only part missing from this full-stack implementation is a connection to the database, which we will add below:
